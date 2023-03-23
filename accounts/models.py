6b309d2344d299a1846fd13a64c48ea3ae989124
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2023-03-24 03:18:06 +0530

    added userProfile Class in model

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20)
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+
     def __str__(self):
-        return self.username
+        return f"{self.user.username}'s profile"
